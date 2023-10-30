7b50c750bc40dbace96de91cb145c23c6b5bf41c
Olliv3r <oliveobom100@gmail.com>
2023-10-30 15:24:44 -0400

    $

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,8 +1,14 @@
-const input = document.querySelector('input[id="link"]');
-const author = document.querySelector('input[id="author"]');
+const author = document.getElementById("author");
+const alias = document.getElementById("alias");
+const nameRepo = document.getElementById("name_repository");
+const link = document.getElementById("link");
+const typeInstall = document.getElementById("type_install");
 
-input.addEventListener('input', update_value);
-
-function update_value() {
-    author.value = input.value
-}
+// Preenche os campos: autor, apelido e nome do repositório dinamicamente
+link.addEventListener("change", (event) => {
+  if (typeInstall.value == "git") {
+    author.value = event.target.value.split("/")[3];
+    alias.value = event.target.value.split("/")[4];
+    nameRepo.value = event.target.value.split("/")[4];
+  }
+});
